c91329139728d0939bbecae6da2c27436f90f5bc
Anusha Tulasi <85635777+anusha2000-cmy@users.noreply.github.com>
2026-06-24 17:51:52 -0700

    Add: counting-bits - submission-1

---
+++ Data Structures & Algorithms/counting-bits/submission-1.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int> dp(n+1,0);
+        if(n>=1){
+            dp[1]=1;
+        }
+        for(int i=1;i<=n;i++){
+            dp[i] = dp[i/2] + dp[i%2];
+        }
+        return dp;
+    }
+};
