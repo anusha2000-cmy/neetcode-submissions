129990ed152049525c7d8a42a67b60db93e0494c
Anusha Tulasi <85635777+anusha2000-cmy@users.noreply.github.com>
2026-06-29 14:09:54 -0700

    Add: maximum-subarray - submission-0

---
+++ Data Structures & Algorithms/maximum-subarray/submission-0.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int res=INT_MIN;
+        int sum=0;
+        for(int num:nums){
+            if(sum+num>num){
+                sum+=num;
+            }else{
+                sum=num;
+            }
+            res=max(res,sum);
+        }
+        return res;
+    }
+};
